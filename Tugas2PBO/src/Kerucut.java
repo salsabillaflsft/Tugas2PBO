f0d2662a8e966f3621a5a578966f59d1a5c7944c
yesyualeon <54468793+yesyualeon@users.noreply.github.com>
2020-04-26 21:18:36 +0700

    Add files via upload

---
+++ Tugas2PBO/src/Kerucut.java
@@ -0,0 +1,42 @@
+public class Kerucut extends Lingkaran {
+
+    float volume_k, lp_kerucut;
+    private float t;
+    private float r;
+
+    @Override
+    public String Cetak2() {
+        return "Volume kerucut : ";
+    }
+
+    void VolumeKerucut(float r,float t) {
+        this.r = r;
+        this.t = t;
+        volume_k = getLuasLingkaran(r) * t / 3;
+    }
+
+    //phi * r * (r + sqrt(r^2 + t^2))
+
+    void LuasPermukaanKerucut(float r, float t) {
+        this.r = r;
+        this.t = t;
+        lp_kerucut = (float) (3.14 * r * (r + Math.sqrt((Math.pow(r))+(Math.pow(t)))));
+    }
+
+    public float NilaiVolumeKerucut() {
+        return volume_k;
+    }
+
+    public float NilaiLuasPermukaanKerucut() {
+        return lp_kerucut;
+    }
+
+    public float get_r() {
+        return r;
+    }
+
+    public float get_t() {
+        return t;
+    }
+
+}
